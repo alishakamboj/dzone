8e75d68f6c9d1384cb3fc29dc502fdeb3d77ff41
Alisha Kamboj <alishakamboj001@gmail.com>
2022-02-05 19:18:48 +0530

    newfiles

---
+++ src/layouts/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Footer !</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer;
